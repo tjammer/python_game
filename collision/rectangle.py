84aff4d808fa53bf482c64f98233320a6a7f3c6f
mokapharr <tobiasjammer@googlemail.com>
2013-12-01 00:10:11 +0100

    added collision detection

--- collision/rectangle.py
+++ collision/rectangle.py
@@ -1,4 +1,5 @@
 import math
+import vector
 
 pi = math.acos(0) * 2
 
@@ -17,6 +18,9 @@ class Rectangle(object):
         self.x4, self.y4 = self.rotated_coord(self.width, 0)
         self.color = color
 
+        self.axis1x, self.axis1y = self.rotate(1, 0)
+        self.axis2x, self.axis2y = self.rotate(0, 1)
+
     def update(self, x, y):
         self.x1, self.y1 = x, y
         self.x2, self.y2 = self.rotated_coord(0, self.height)
@@ -24,9 +28,13 @@ class Rectangle(object):
         self.x4, self.y4 = self.rotated_coord(self.width, 0)
 
     def collides(self, rect):
-        pass
+        return vector.collides(self, rect)
 
-    def rotated_coord(self, x, y):
+    def rotate(self, x, y):
         x_ = x * math.cos(self.angle) - y * math.sin(self.angle)
         y_ = x * math.sin(self.angle) + y * math.cos(self.angle)
+        return x_, y_
+
+    def rotated_coord(self, x, y):
+        x_, y_ = self.rotate(x, y)
         return self.x1 + x_, self.y1 + y_
---
+++ collision/vector.py
@@ -0,0 +1,64 @@
+import math
+
+"""vector functions and general help functions for rectangle
+and collision detection"""
+
+
+def magnitude(x, y):
+    return math.sqrt(x**2 + y**2)
+
+
+def dot_product(x1, y1, x2, y2):
+    return (x1 * x2 + y1 * y2)
+
+
+def min_max(points, axis):
+    mx = max(dot_product(point[0], point[1], axis[0],
+                         axis[1]) for point in points)
+    mn = max(dot_product(point[0], point[1], axis[0],
+                         axis[1]) for point in points)
+    return mn, mx
+
+
+def overlap(a_min, a_max, b_min, b_max):
+    if not (a_max < b_min or b_max < a_min):
+        return True, min(a_max - b_min, b_max - a_min)
+
+
+def collides(rect1, rect2):
+
+    # s_points are own points, r_points for other rect2
+    s_points = [[rect1.x1, rect1.y1], [rect1.x2, rect1.y2],
+                [rect1.x3, rect1.y3], [rect1.x4, rect1.y4]]
+    r_points = [[rect2.x1, rect2.y1], [rect2.x2, rect2.y2],
+                [rect2.x3, rect2.y3], [rect2.x4, rect2.y4]]
+
+    # four axis
+    s_axis1 = [rect1.axis1x, rect1.axis1y]
+    s_axis2 = [rect1.axis2x, rect1.axis2y]
+    r_axis1 = [rect2.axis1x, rect2.axis1y]
+    r_axis2 = [rect2.axis2x, rect2.axis2y]
+
+    # first check
+    s_min, s_max = min_max(s_points, [s_axis1])
+    r_min, r_max = min_max(r_points, [s_axis1])
+    ovr1 = overlap(s_min, s_max, r_min, s_max) + (s_axis1,)
+    if ovr1[0]:
+        # second check
+        s_min, s_max = min_max(s_points, [s_axis2])
+        r_min, r_max = min_max(r_points, [s_axis2])
+        ovr2 = overlap(s_min, s_max, r_min, s_max) + (s_axis2,)
+        if ovr2[0]:
+            s_min, s_max = min_max(s_points, [r_axis1])
+            r_min, r_max = min_max(r_points, [r_axis1])
+            ovr3 = overlap(s_min, s_max, r_min, s_max) + (r_axis1,)
+            if ovr3[0]:
+                s_min, s_max = min_max(s_points, [r_axis2])
+                r_min, r_max = min_max(r_points, [r_axis2])
+                ovr4 = overlap(s_min, s_max, r_min, s_max) + (r_axis2,)
+                if ovr4[0]:
+                    ovr_list = [ovr1, ovr2, ovr3, ovr4]
+                    ovrlap = min(i[1] for i in ovr_list)
+                    min_axis = [i[2] for i in ovr_list if ovrlap in i][0]
+
+                    return ovrlap, min_axis
