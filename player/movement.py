6dd3d228a24af86aba77beff94ef797927fb882c
mokapharr <tobiasjammer@googlemail.com>
2014-05-27 22:34:16 +0200

    changed movement to take external state and input to make client side prediction possible

--- player/movement.py
+++ player/movement.py
@@ -19,41 +19,43 @@ class Movement(object):
 
         self.input = proto.input()
 
-    def update(self, dt, pos):
-        self.calc_vel(dt)
-        self.step(dt, pos)
+    def update(self, dt, pos, vel, input=None):
+        if input is None:
+            input = self.input
+        self.calc_vel(dt, pos, vel, input)
+        self.step(dt, pos, vel)
         self.set_conds()
         return self.vel, self.pos
 
-    def step(self, dt, pos):
+    def step(self, dt, pos, vel):
         if not self.conds['on_ground']:
-            self.vel[1] -= self.gravity * dt
+            self.vel[1] = vel[1] - self.gravity * dt
         for i, j in enumerate(self.pos):
             self.pos[i] = pos[i] + self.vel[i] * dt
 
-    def calc_vel(self, dt):
+    def calc_vel(self, dt, pos, vel, input):
         #check left right
-        if self.input.right and not self.input.left:
+        if input.right and not input.left:
             sign = 1
-        elif self.input.left and not self.input.right:
+        elif input.left and not input.right:
             sign = -1
         else:
             self.vel[0] = 0
             sign = 0
-        self.curr_sign = self.sign_of(self.vel[0])
+        self.curr_sign = self.sign_of(vel[0])
         v = self.normal_accel
         if self.curr_sign != 0 and self.curr_sign != sign:
             v *= self.turn_multplier
-        self.vel[0] += v * sign * dt
-        self.vel[1] += v * math.tan(self.angle) * sign * dt
-        if abs(self.vel[0]) > self.max_vel:
+        self.vel[0] = vel[0] + v * sign * dt
+        self.vel[1] = vel[1] + v * math.tan(self.angle) * sign * dt
+        if abs(vel[0]) > self.max_vel:
             self.vel[0] = self.max_vel * self.curr_sign
         #check jump
-        if self.conds['can_jump'] and self.input.up:
+        if self.conds['can_jump'] and input.up:
             self.vel[1] = self.jump_vel
             self.conds['can_jump'] = False
         #in air
-        if not self.input.up and self.vel[1] > 0:
+        if not input.up and self.vel[1] > 0:
             self.vel[1] = 0
 
     def set_conds(self):
