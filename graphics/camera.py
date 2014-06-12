527601ca3b375fbf461c21f247476516bcfbcb91
mokapharr <tobiasjammer@googlemail.com>
2014-06-12 20:29:44 +0200

    changed camera to also use y axis

--- graphics/camera.py
+++ graphics/camera.py
@@ -1,46 +1,47 @@
 from pyglet.gl import *
+from menu.menu_events import Events
+from player.state import vec2
 
 
-class Camera(object):
+class Camera(Events):
     """docstring for Camera"""
     def __init__(self, window):
         super(Camera, self).__init__()
-        self.x = - 2 * window.width
-        self.y = window.height / 2
-        self.target_x = self.x
-        self.target_y = self.y
+        self.campos = vec2(- 2 * window.width, window.height / 2)
+        self.target = vec2(self.campos.x, self.campos.y)
         self.h = window.height / 2
         self.w = window.width / 2
-        self.p_pos = 0
-        self.mul_easing = .3
-        self.m_x = 1280 / 2
-        self.m_y = 720 / 2
+        self.pos = vec2(0, 0)
+        self.vel = vec2(0, 0)
+        self.mul_easing = .3 * 30
+        self.mpos = vec2(self.w, self.h)
         # y offset to have player in the lower half of the screen
-        self.y_offset = -120
-        self.eas_vel = 0
+        self.offset = vec2(0, -self.h / 2)
+        self.eas_vel = vec2(0, 0)
 
     def update(self, dt, state):
-        self.p_pos, self.p_vel = state.pos.x, state.vel.x
+        self.pos, self.vel = state.pos, state.vel
         # velocity easing
-        if self.p_vel != 0:
-            self.eas_vel -= (self.eas_vel - self.p_vel) * dt * 4.5
-        self.target_x = self.m_x + self.p_pos + self.eas_vel * 0.5
-        self.x -= (self.x - self.target_x) * self.mul_easing * dt * 30
-        self.target_y = self.m_y + self.y_offset
-        self.y -= (self.y - self.target_y) * self.mul_easing * dt * 30
+        if self.vel.x != 0:
+            self.eas_vel.x -= (self.eas_vel.x - self.vel.x) * dt * 4.5
+        #self.eas_vel.y -= (self.eas_vel.y - self.vel.y) * dt
+        self.target = self.mpos + self.pos + self.eas_vel * 0.5 + self.offset
+        self.campos -= (self.campos - self.target) * self.mul_easing * dt
+        #print self.x + self.m_x - 2 * self.w
 
     def set_camera(self):
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
-        glTranslatef(-self.x + 2 * self.w,
-                     -self.y + self.h, 0)
+        glTranslatef(-self.campos.x + 2 * self.w,
+                     -self.campos.y + self.h, 0)
 
     def set_static(self):
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
 
-    def receive_player_pos(self, event, msg):
-        self.p_pos, self.p_vel = msg[0], msg[1]
-
     def receive_m_pos(self, event, msg):
-        self.m_x, self.m_y = msg[0], msg[1]
+        self.mpos.x, self.mpos.y = msg[0], msg[1]
+
+    def on_resize(self, width, height):
+        self.h = height / 2
+        self.width = width / 2
