1be4dcd87d603ea6ac4c0ab385b14c339eb4e5bd
mokapharr <tobiasjammer@googlemail.com>
2014-07-06 19:23:36 +0200

    updated movement to cancel bunnyhop

--- player/movement.py
+++ player/movement.py
@@ -15,12 +15,13 @@ class Movement(object):
         self.jump_vel = 900.
         self.max_vel = 500
         self.angle = 0
-        self.conds = {'can_jump': False, 'on_ground': False}
+        self.conds = {'can_jump': False, 'on_ground': False, 'rel_jump': True}
+        self.groundtime = 0
 
     def update(self, dt, pos, vel, input):
         self.calc_vel(dt, pos, vel, input)
         self.step(dt, pos, vel)
-        self.set_conds()
+        self.set_conds(dt)
         return self.vel, self.pos
 
     def step(self, dt, pos, vel):
@@ -38,24 +39,38 @@ class Movement(object):
             self.vel[0] = 0
             sign = 0
         self.curr_sign = self.sign_of(vel[0])
-        v = self.normal_accel
+        if not abs(vel[0]) >= self.max_vel:
+            v = self.normal_accel
+        else:
+            v = 0
         if self.curr_sign != 0 and self.curr_sign != sign:
             v *= self.turn_multplier
+        avel = abs(vel.x)
+        cond1 = avel < self.max_vel and avel + v * dt > self.max_vel
+        cond2 = avel > self.max_vel and self.groundtime > 0.1
+        if cond1 or cond2:
+            self.vel.x = self.max_vel * self.curr_sign
+            v = 0
         self.vel[0] = vel[0] + v * sign * dt
-        #self.vel[1] = vel[1] + v * math.tan(self.angle) * sign * dt
-        if abs(vel[0]) > self.max_vel:
-            self.vel[0] = self.max_vel * self.curr_sign
         #check jump
-        if self.conds['can_jump'] and input.up:
+        if self.conds['can_jump'] and self.conds['rel_jump'] and input.up:
             self.vel[1] = self.jump_vel
             self.conds['can_jump'] = False
+            self.conds['rel_jump'] = False
+            self.conds['on_ground'] = False
         #in air
         if not input.up and self.vel[1] > 0:
             self.vel[1] = 0
+        #check if jump is released
+        if not input.up:
+            self.conds['rel_jump'] = True
 
-    def set_conds(self):
+    def set_conds(self, dt):
         if self.conds['on_ground']:
             self.conds['can_jump'] = True
+            self.groundtime += dt
+        else:
+            self.groundtime = 0
         self.conds['on_ground'] = False
 
     def sign_of(self, num):
