31c8213ddaccc1204943b52c48c17cde384872d3
mokapharr <tobiasjammer@googlemail.com>
2014-11-25 14:06:52 +0100

    added vec3 class

---
+++ graphics/vec3.pyx
@@ -0,0 +1,62 @@
+from libc.math cimport sqrt
+
+cdef class vec3:
+    cdef public float x, y, z
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __iter__(self):
+        return iter((self.x, self.y, self.z))
+
+    def __repr__(self):
+        return ', '.join((str(self.x), str(self.y), str(self.z)))
+
+    def __sub__(self, vec):
+        if isinstance(vec, vec3):
+            return vec3(self.x - vec.x, self.y - vec.y, self.z - vec.z)
+        else:
+            raise TypeError
+
+    def __add__(self, vec):
+        if isinstance(vec, vec3):
+            return vec3(self.x + vec.x, self.y + vec.y, self.z + vec.z)
+        else:
+            raise TypeError
+
+    def __mul__(self, num):
+        if isinstance(num, float) or isinstance(num, int):
+            return vec3(self.x * num, self.y * num, self.z * num)
+        else:
+            raise TypeError
+
+    def __div__(self, num):
+        if isinstance(num, float) or isinstance(num, int):
+            return vec3(self.x / num, self.y / num, self.z / num)
+        else:
+            raise TypeError
+
+    def __richcmp__(self, other, int op):
+        if isinstance(other, vec3):
+            if op == 2:
+                if other.x == self.x:
+                    if other.y == self.y:
+                        if other.z == self.z:
+                            return True
+                return False
+            elif op == 3:
+                if other.x == self.x:
+                    if other.y == self.y:
+                        if other.z == self.z:
+                            return False
+                return True
+            else:
+                raise TypeError
+        else:
+            raise TypeError
+
+    def mag(self):
+        """magnitude of the vector"""
+        return sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
